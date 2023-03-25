1afd82c3addbda82849022e219e154a21ba54de6
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-25 16:08:57 +0000

    main function

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main - is the entry point .
+ * all functions of my project will be tested in main
+ * Return: 0 code is good .
+ *
+ */
+
+
+int main(void)
+{
+  /* Functions should be here*/
+
+  
+  return (0);
+}
